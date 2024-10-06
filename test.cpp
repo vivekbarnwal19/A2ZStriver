7084fac83096748e69acd7d26449ab6258688eae
Vivek Kumar Barnwal <vivekbarnwal2001@gmail.com>
2024-10-07 01:21:49 +0530

    test

---
+++ test.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // cout<<"radha"<<endl;
+    // cout<<"govinda";
+    // int x;
+    // int y;
+    // cin>>x>>y;
+    // cout<<"value of the item is "<<x <<"and y is "<<y<<endl;
+
+    //   int x;
+    // int y;
+    // cin >> x >> y;
+    // cout << "Value of x: " << x << " and y: " <<y;
+    // return 0;
+// int age=20;
+//     if (age>=18){
+//         cout<<"age si greater"<<endl;
+//     }else{
+//         cout<<"age is lesser"<<endl;
+// }
+
+int day=3;
+switch (day)
+{
+case 3:
+    cout<<"Monday";
+    break;
+
+default:
+    break;
+}
+
+}
