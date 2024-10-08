19a06f9a57a8545ff4cbe569f85cae9d182898d1
Vivek Kumar Barnwal <vivekbarnwal2001@gmail.com>
2024-10-08 23:47:33 +0530

    patternQue

--- Patterns/pattern6.cpp
+++ Patterns/pattern6.cpp
@@ -4,9 +4,17 @@ int main(){
     int n;
     cin>>n;
     for(int i=0;i<=n;i++){
-        for(int j=n;j>=i;j--){
-            cout<< (n-j) <<" ";
+        for(int j=n;j>i;j--){
+            cout<< (n-j+1) <<" ";
         }
         cout<<endl;
     }
 }
+
+
+// 5
+// 1 2 3 4 5 
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
